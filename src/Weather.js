e83154539c54123003b385599a1ee11cfe9dae9d
mozhijano <40516403+mozhijano@users.noreply.github.com>
2023-02-21 17:57:11 +0100

    add search engine option and convert temperature to celsius and fahrenheit

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import WeatherTemperature from "./WeatherTempreture";
 import "./App.css";
 
-function Weather() {
-  let [city, setCity] = useState([
-    { id: 1, name: "Lisbon" },
-    { id: 2, name: "Paris" },
-    { id: 3, name: "Sydney" },
-    { id: 4, name: "San Francisco" },
-  ]);
-  let [weather, setWeather] = useState({});
+function Weather(props) {
+  let [city, setCity] = useState(props.defaultCity);
+  // [
+  //   { id: 1, name: "Lisbon" },
+  //   { id: 2, name: "Paris" },
+  //   { id: 3, name: "Sydney" },
+  //   { id: 4, name: "San Francisco" },
+  // ];
+  let [weather, setWeather] = useState({ loaded: false });
 
-  function getCurrentCityWeather(cityName) {
+  function getCurrentCityWeather() {
     let apiKey = "a78075e9e54bb7f4634858f9d04d965c";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(url).then(showTemperature);
   }
   function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
     let hours = date.getHours();
+    let minutes = date.getMinutes();
     let days = [
       "Sunday",
       "Monday",
@@ -29,11 +32,18 @@ function Weather() {
       "Friday",
       "Saturday",
     ];
-    return days[day] + " " + hours + ":00";
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${hours}`;
+    }
+    return `${days[day]} ${hours}:${minutes}`;
   }
 
   function showTemperature(response) {
     setWeather({
+      loaded: true,
       name: response.data.name,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
@@ -46,18 +56,18 @@ function Weather() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    getCurrentCityWeather(event.target.innerText);
+    getCurrentCityWeather();
   }
 
   function getCityName(event) {
     setCity(event.target.value);
   }
 
-  useEffect(() => {
-    let apiKey = "a78075e9e54bb7f4634858f9d04d965c";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
-    axios.get(url).then(showTemperature);
-  }, []);
+  // useEffect(() => {
+  //   let apiKey = "a78075e9e54bb7f4634858f9d04d965c";
+  //   let url = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+  //   axios.get(url).then(showTemperature);
+  // }, []);
 
   let form = (
     <form className="form-css" id="search-form" onSubmit={handleSubmit}>
@@ -83,84 +93,87 @@ function Weather() {
       </div>
     </form>
   );
-
-  return (
-    <div className="container">
-      <div className="weather">
-        {city.map((c, index) => (
+  if (weather.loaded) {
+    return (
+      <div className="container">
+        <div className="weather">
+          {/* {city.map((c, index) => (
           <span className="city" key={index}>
             <a href="/" onClick={handleSubmit} id={c.id}>
               {c.name}
             </a>
           </span>
-        ))}
-        <div className="row">
-          <div className="col">{form}</div>
-        </div>
-        <div className="row">
-          <div className="col wt-desc text-start mt-5">
-            <div className="row">
-              <div className="col city-name" id="city-name">
-                {weather.name}
+        ))} */}
+          <div className="row">
+            <div className="col">{form}</div>
+          </div>
+          <div className="row">
+            <div className="col wt-desc text-start mt-5">
+              <div className="row">
+                <div className="col city-name" id="city-name">
+                  {weather.name}
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col details" id="current-day">
-                {formatDay(weather.currentDay)}
+              <div className="row">
+                <div className="col details" id="current-day">
+                  {formatDay(weather.currentDay)}
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col details" id="weather-description">
-                {weather.description}
+              <div className="row">
+                <div className="col details" id="weather-description">
+                  {weather.description}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row weather-info">
-          <div className="col-1">
-            <img src={weather.icon} alt={weather.description} />
-          </div>
-          <div className="col-7 text-start">
-            <span className="degree-num" id="weather-degree"></span>
-            <span className="text-start degree-sign">
-              <a id="celsius">{Math.round(weather.temperature)}°C</a>|
-              <a id="fahrenheit">°F</a>
-            </span>
-          </div>
-          <div className="col-3 wt-desc text-start">
-            <div className="row">
-              <div className="col details">
-                Humidity:
-                <span className="temp-relations">
-                  <span id="humidity">{weather.humidity}</span>%
-                </span>
+          <div className="row weather-info ">
+            <div className="col-6 mt-3">
+              <div className="d-flex">
+                <div>
+                  <img src={weather.icon} alt={weather.description} />
+                </div>
+                <div>
+                  <WeatherTemperature celsius={weather.temperature} />
+                </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col details">
-                Wind:
-                <span className="temp-relations">
-                  <span id="weather-wind">{weather.wind}</span> km/h
-                </span>
+            <div className="col-6 wt-desc text-start">
+              <div className="row">
+                <div className="col details">
+                  Humidity:
+                  <span className="temp-relations">
+                    <span id="humidity">{weather.humidity}</span>%
+                  </span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col details">
+                  Wind:
+                  <span className="temp-relations">
+                    <span id="weather-wind">{weather.wind}</span> km/h
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div id="forecast" className="mt-5"></div>
-        <div className="footer">
-          <a
-            href="https://github.com/mozhijano/Weather-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>
-          by Mozhgan Janoosepah
+          <div id="forecast" className="mt-5"></div>
+          <div className="footer">
+            <a
+              href="https://github.com/mozhijano/Weather-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>
+            by Mozhgan Janoosepah
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    getCurrentCityWeather();
+  }
 }
 
 export default Weather;
